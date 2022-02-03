9c504f7da4c6fec9864c5c791c4a4dbc01cc8c31
hrukalive <bowen.parallife@gmail.com>
2022-02-02 23:28:00 -0600

    Clean branch

--- DMG/frame_classification/WeSTClass11/test2.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES=1
-
-#  --trained_weights /home/dmg/CP4/news_match/WeSTClass/results/news/cnn/phase3/final.h5
-
-/home/qiuwenda/anaconda3/envs/west/bin/python main.py --dataset news_manual_full --sup_source keywords --model cnn --with_evaluation False
