2ee1a5ea52f89cdc2b1728120bfb52da4f0e8586
hrukalive <bowen.parallife@gmail.com>
2022-03-06 16:12:07 -0600

    Fix

--- misc/timeseries.sh
+++ misc/timeseries.sh
@@ -2,31 +2,32 @@
 
 DATA_ROOT="/data/socialsim_data"
 START_DATE="2020-02-01"
-END_DATE="2021-02-01"
+FULL_END_DATE="2021-02-01"
+TRAIN_END_DATE="2021-01-11"
 
 python preprocess_extracted_gt.py \
     -i "$DATA_ROOT/Challenge/eval-2/extracted_ground_truth/cp6.ea.gte.twitter-youtube.2020-11-30_2020-12-20.json.gz" \
-    -s "$START_DATE" -e "$END_DATE" \
+    -s "$START_DATE" -e "$FULL_END_DATE" \
     -o "$DATA_ROOT/workdir/"
 python preprocess_extracted_gt.py \
     -i "$DATA_ROOT/Challenge/eval-3/extracted_ground_truth/cp6.ea.gte.twitter-youtube.2020-12-21_2021-01-10.json.gz" \
-    -s "$START_DATE" -e "$END_DATE" \
+    -s "$START_DATE" -e "$FULL_END_DATE" \
     -o "$DATA_ROOT/workdir/"
 
 python timeseries_gdelt.py \
     -i "$DATA_ROOT/Exogenous/GDELT/cp6.ea.gdelt.events.v1.json.gz" \
     -g D \
-    -s "$START_DATE" -e "$END_DATE" \
+    -s "$START_DATE" -e "$FULL_END_DATE" \
     -o "$DATA_ROOT/workdir/cp6_gdelt_timeseries.json"
 python timeseries_acled.py \
     -i "$DATA_ROOT/Exogenous/ACLED/cp6.ea.acled.v1.csv.gz" \
     -g D \
-    -s "$START_DATE" -e "$END_DATE" \
+    -s "$START_DATE" -e "$FULL_END_DATE" \
     -o "$DATA_ROOT/workdir/cp6_acled_timeseries.json"
 python timeseries_news_src.py \
     -i "$DATA_ROOT/Exogenous/NewsArticles/cp6.ea.newsarticles.training.v1.json" \
     -g D -t 366 \
-    -s "$START_DATE" -e "$END_DATE" \
+    -s "$START_DATE" -e "$FULL_END_DATE" \
     -o "$DATA_ROOT/workdir/cp6_newssrc_timeseries.json"
 
 python timeseries_platform.py \
@@ -34,14 +35,15 @@ python timeseries_platform.py \
        "$DATA_ROOT/workdir/cp6.ea.gte.twitter-youtube.2020-12-21_2021-01-10.json_twitter.json" \
     -n "$DATA_ROOT/cp6_twitter_nodelist.txt" \
     -p "twitter" \
-    -s "$START_DATE" -e "$END_DATE" \
+    -s "$START_DATE" -e $TRAIN_END_DATE \
     -o "$DATA_ROOT/workdir/cp6_twitter_timeseries.json" \
     --statout "$DATA_ROOT/workdir/cp6_twitter_prob.json"
+
 python timeseries_platform.py \
     -i "$DATA_ROOT/workdir/cp6.ea.gte.twitter-youtube.2020-11-30_2020-12-20.json_youtube.json" \
        "$DATA_ROOT/workdir/cp6.ea.gte.twitter-youtube.2020-12-21_2021-01-10.json_youtube.json" \
     -n "$DATA_ROOT/cp6_twitter_nodelist.txt" \
     -p "youtube" \
-    -s "$START_DATE" -e "$END_DATE" \
+    -s "$START_DATE" -e $TRAIN_END_DATE \
     -o "$DATA_ROOT/workdir/cp6_youtube_timeseries.json" \
     --statout "$DATA_ROOT/workdir/cp6_youtube_prob.json"
